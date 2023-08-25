87ef04b96e716f203f7b93951d34801d9b1e8bef
ThaiDuy12345 <nguyenngocthaiduy.nt@gmail.com>
2023-08-25 11:05:59 +0700

    update

--- src/main/java/com/duan/Entity/AccountEntity.java
+++ src/main/java/com/duan/Entity/AccountEntity.java
@@ -1,5 +1,4 @@
 package com.duan.Entity;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
--- src/main/java/com/duan/checkconnectDTB/check.java
+++ src/main/java/com/duan/checkconnectDTB/check.java
@@ -1,6 +1,5 @@
  package com.duan.checkconnectDTB;
 
- import javax.sql.DataSource;
  import java.sql.Connection;
  import java.sql.DriverManager;
  import java.sql.SQLException;

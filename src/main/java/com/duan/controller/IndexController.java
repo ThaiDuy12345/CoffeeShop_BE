7af0de4c213a1ea529788f9c288331384101bf97
ThaiDuy12345 <nguyenngocthaiduy.nt@gmail.com>
2023-10-15 19:00:58 +0700

    update

--- src/main/java/com/duan/controller/IndexController.java
+++ src/main/java/com/duan/controller/IndexController.java
@@ -2,22 +2,21 @@ package com.duan.controller;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class IndexController {
 
   @GetMapping("/")
-  public ResponseEntity<Map<String, String>> getServerStatus(){
-    Map<String, String> response = new HashMap<>();
-    
+  public ResponseEntity<Map<String, Object>> getServerStatus(){
+    Map<String, Object> response = new HashMap<>();
     response.put("status", "true");
     response.put("message", "Server is working at port 8080!!");
-
     return ResponseEntity.ok(response);
-
   }
 }
 

1513fa6d2b2b8058e2483f70d6b2e7ae5b1e8bfa
KhanhAn02072002 <ankhanh656@gmail.com>
2023-10-17 22:32:39 +0700

    Update AccountController.java

--- src/main/java/com/duan/controller/AccountController.java
+++ src/main/java/com/duan/controller/AccountController.java
@@ -1,20 +1,15 @@
 package com.duan.controller;
 
 import java.util.List;
-import java.util.Optional;
-
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.duan.entity.AccountEntity;
 import com.duan.repository.AccountRepository;
 
@@ -27,74 +22,65 @@ public class AccountController {
     public AccountController(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
     }
- // GET /accounts
-    @GetMapping
-    public List<AccountEntity> getAllAccounts() {
-        return accountRepository.findAll();
-    }
- // GET /accounts/{phone}
-    @GetMapping("/{phone}")
-    public ResponseEntity<AccountEntity> getAccountByPhone(@PathVariable String phone) {
-        Optional<AccountEntity> optionalAccount = accountRepository.findById(phone);
-        if (optionalAccount.isPresent()) {
-            return ResponseEntity.ok(optionalAccount.get());
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
+    @RequestMapping(value = "", method = RequestMethod.GET)
+	public ResponseEntity<List<AccountEntity>> listAllAccount(){
+		List<AccountEntity> listaccount= accountRepository.findAll();
+		if(listaccount.isEmpty()) {
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<List<AccountEntity>>(listaccount, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/{phone}", method = RequestMethod.GET)
+	public AccountEntity findAccount(@PathVariable("phone") int accountPhone) {
+		AccountEntity account= accountRepository.getOne(accountPhone);
+		if(account == null) {
+			ResponseEntity.notFound().build();
+		}
+		return account;
+	}
+	
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public AccountEntity saveContact(@Valid @RequestBody AccountEntity account) {
+		return accountRepository.save(account);
+	}
+	
+	@RequestMapping(value = "", method = RequestMethod.PUT)
+	public ResponseEntity<AccountEntity> updateContact(@PathVariable(value = "phone") int accountPhone, 
+	                                       @Valid @RequestBody AccountEntity accountForm) {
+		AccountEntity account = accountRepository.getOne(accountPhone);
+	    if(account == null) {
+	        return ResponseEntity.notFound().build();
+	    }
+	    account.setAccountName(accountForm.getAccountName());
+	    account.setAccountPassword(accountForm.getAccountPassword());
+	    account.setAccountAddress(accountForm.getAccountAddress());
+	    account.setAccountEmail(accountForm.getAccountEmail());
+	    account.setAccountRole(accountForm.getAccountRole());
+	    account.setAccountActive(true);
 
-    @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody AccountEntity account) {
-    	if (accountRepository.existsByPhone(account.getAccountPhone())) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Phone already exists");
-        }
-        // Insert new user into the database
-    	try {
-            accountRepository.save(account);
-            return ResponseEntity.ok("User registered successfully");
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error registering user");
-        }
-    }
- // PUT /accounts/{phone}
-    @PutMapping("/{phone}")
-    public ResponseEntity<String> updateAccount(@PathVariable String phone, @RequestBody AccountEntity account) {
-        Optional<AccountEntity> optionalAccount = accountRepository.findById(phone);
-        if (optionalAccount.isPresent()) {
-            AccountEntity existingAccount = optionalAccount.get();
-            
-            // Update properties of the existing account
-            existingAccount.setAccountName(account.getAccountName());
-            existingAccount.setAccountPassword(account.getAccountPassword());
-            existingAccount.setAccountAddress(account.getAccountAddress());
-            existingAccount.setAccountEmail(account.getAccountEmail());
-            existingAccount.setAccountRole(account.getAccountRole());
-            existingAccount.setAccountActive(account.isAccountActive());
-            
-            accountRepository.save(existingAccount);
-            return ResponseEntity.ok("Account updated successfully");
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
- // DELETE /accounts/{phone}
-    @DeleteMapping("/{phone}")
-    public ResponseEntity<String> deleteAccount(@PathVariable String phone) {
-        if (accountRepository.existsById(phone)) {
-            accountRepository.deleteById(phone);
-            return ResponseEntity.ok("Account deleted successfully");
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
-    @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody AccountEntity account) {
+	    AccountEntity updatedAccount = accountRepository.save(account);
+	    return ResponseEntity.ok(updatedAccount);
+	}
+	
+	@RequestMapping(value = "/{phone}", method = RequestMethod.DELETE)
+	public ResponseEntity<AccountEntity> deleteContact(@PathVariable(value = "phone") int accountPhone) {
+		AccountEntity account = accountRepository.getOne(accountPhone);
+	    if(account == null) {
+	        return ResponseEntity.notFound().build();
+	    }
+
+	    accountRepository.delete(account);
+	    return ResponseEntity.ok().build();
+	}
+	@RequestMapping(value = "/{login}", method = RequestMethod.POST)
+	public ResponseEntity<String> login(@RequestBody AccountEntity account) {
         AccountEntity foundAccount = accountRepository.findByEmailAndPassword(account.getAccountEmail(), account.getAccountPassword());
 
         if (foundAccount == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
+        	return ResponseEntity.notFound().build();
         }
-
-        return ResponseEntity.ok("Login successful");
+        accountRepository.save(account);
+	    return ResponseEntity.ok().build();
     }
 }

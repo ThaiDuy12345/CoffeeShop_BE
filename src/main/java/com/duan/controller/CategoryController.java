17f8c4f00e7c8431fad11ed732e68753b25d840d
ThaiDuy12345 <nguyenngocthaiduy.nt@gmail.com>
2023-10-24 21:59:21 +0700

    d

--- src/main/java/com/duan/controller/CategoryController.java
+++ src/main/java/com/duan/controller/CategoryController.java
@@ -4,12 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.duan.entity.AccountEntity;
 import com.duan.entity.CategoryEntity;
 import com.duan.repository.CategoryRepositoty;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
